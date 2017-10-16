46f65997060b5379807a542e7fb9dc4d8605a62f
Vladimir Rodchenko <vladimir@aurochses.com>
2017-10-16 21:22:08 +0300

    initial code

---
+++ test/Aurochses.Module.Autofac.EntityFramework.Tests/ModuleBaseTests.cs
@@ -0,0 +1,38 @@
+﻿using Xunit;
+
+namespace Aurochses.Module.Autofac.EntityFramework.Tests
+{
+    public class ModuleBaseTests
+    {
+        private const string NameOrConnectionString = "Database";
+        private const string SchemaName = "dbo";
+
+        private readonly ModuleBase _moduleBase;
+
+        public ModuleBaseTests()
+        {
+            _moduleBase = new ModuleBase(NameOrConnectionString, SchemaName);
+        }
+
+        [Fact]
+        public void Inherit_Module()
+        {
+            // Arrange & Act & Assert
+            Assert.IsAssignableFrom<global::Autofac.Module>(_moduleBase);
+        }
+
+        [Fact]
+        public void NameOrConnectionString_Get_Success()
+        {
+            // Arrange & Act & Assert
+            Assert.Equal(NameOrConnectionString, _moduleBase.NameOrConnectionString);
+        }
+
+        [Fact]
+        public void SchemaName_Get_Success()
+        {
+            // Arrange & Act & Assert
+            Assert.Equal(SchemaName, _moduleBase.SchemaName);
+        }
+    }
+}
